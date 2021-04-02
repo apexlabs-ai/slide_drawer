7a715d3611cdbee299e0a2ac6fec922be5ae63e9
Alexis Yushin <alexis@ww.net>
2021-04-02 10:20:28 +0200

    null safety + curved animation controller

--- lib/src/container.dart
+++ lib/src/container.dart
@@ -1,44 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:slide_drawer/src/alignment.dart';
-import 'package:slide_drawer/src/item.dart';
+
+import '../slide_drawer.dart';
 
 class SlideDrawerContainer extends StatelessWidget {
 
-  final Widget drawer;
-  final Widget head;
-  final Widget content;
-  final List<MenuItem> items;
+  final Widget? drawer;
+  final Widget? head;
+  final Widget? content;
+  final List<MenuItem>? items;
   final double paddingRight;
 
   /// The gradient to use for the background.
   ///
   /// If this property is null, then [background] is used.
-  final Gradient backgroundGradient;
+  final Gradient? backgroundGradient;
 
   /// The color to use for the background. Typically this should be set
   /// along with [brightness].
   ///
   /// If this property is null, then [ThemeData.primaryColor] is used.
-  final Color backgroundColor;
+  final Color? backgroundColor;
 
   /// The brightness of the app bar's material. Typically this is set along
   /// with [backgroundColor], [backgroundGradient].
   ///
   /// If this property is null, then [ThemeData.primaryColorBrightness] is used.
-  final Brightness brightness;
+  final Brightness? brightness;
 
   /// Vertical alignment of content inside [SlideDrawerContainer] 
   /// it can [start] from the top, or [center]
-  final SlideDrawerAlignment alignment;
+  final SlideDrawerAlignment? alignment;
 
-  bool get _hasItems => items != null && items.isNotEmpty;
+  bool get _hasItems => items != null && items!.isNotEmpty;
   bool get _hasDrawer => drawer != null;
   bool get _hasHead => head != null;
   bool get _hasContent => content != null;
   bool get _hasGradient => backgroundGradient != null;
 
   SlideDrawerContainer({
-    Key key, 
+    Key? key, 
     this.drawer,
     this.head, 
     this.content, 
@@ -54,7 +54,7 @@ class SlideDrawerContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     
     ThemeData theme = Theme.of(context);
-    SlideDrawerAlignment _alignment = alignment;
+    SlideDrawerAlignment? _alignment = alignment;
 
     if(_alignment == null) {
       _alignment = _hasHead 
@@ -77,13 +77,13 @@ class SlideDrawerContainer extends StatelessWidget {
                 ? MainAxisAlignment.start : MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                if(_hasHead) head,
+                if(_hasHead) head!,
                 if(_hasContent) Container(
                   margin: EdgeInsets.only(right: paddingRight),
                   child: content,
                 ),
                 if(!_hasContent && _hasItems) 
-                  for(MenuItem item in items) Container(
+                  for(MenuItem item in items!) Container(
                     margin: EdgeInsets.only(right: paddingRight),
                     child: MenuItemWidget(item: item),
                   ),
@@ -97,13 +97,13 @@ class SlideDrawerContainer extends StatelessWidget {
 }
 
 class MenuItemWidget extends StatelessWidget {
-  final MenuItem item;
+  final MenuItem? item;
 
-  MenuItemWidget({Key key, this.item}) : super(key: key);
+  MenuItemWidget({Key? key, this.item}) : super(key: key);
 
-  Widget get _leading {
-    if(item.hasLeading) return item.leading;
-    if(item.hasIcon) return Icon(item.icon);
+  Widget? get _leading {
+    if(item!.hasLeading) return item!.leading;
+    if(item!.hasIcon) return Icon(item!.icon);
     return null;
   }
 
@@ -112,8 +112,8 @@ class MenuItemWidget extends StatelessWidget {
     return ListTile(
       leading: _leading,
       contentPadding: EdgeInsets.only(left: _leading == null ? 24 : 16),
-      title: Text(item.title),
-      onTap: item.onTap,
+      title: Text(item!.title),
+      onTap: item!.onTap as void Function()?,
     );
   }
 }
